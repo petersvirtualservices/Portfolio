099d87b36c3f2ca7ca087784820889f05ce1d470
Charlene Peters <cpeters1402@gmail.com>
2021-06-18 22:50:03 -0500

    Update Project Page

--- src/components/main.js
+++ src/components/main.js
@@ -4,6 +4,8 @@ import {Switch, Route} from 'react-router-dom';
 import AboutMe from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
+import img from '../assets/images/SocialMediaPhoto.png';
+import Image from 'react-bootstrap/Image';
 
 const Main = () => (
     <Switch>
@@ -11,6 +13,7 @@ const Main = () => (
         <Route path="/aboutme" component={AboutMe} />
         <Route path="/projects" component={Projects} />
         <Route path="/contact" component={Contact} />
+        <Image src={img} />
     </Switch>
 )
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,57 +1,93 @@
 import React, { Component } from 'react';
-import { Card, Button, CardDeck } from 'react-bootstrap';
+import { Card, CardGroup, CardDeck } from 'react-bootstrap';
 import img1 from '../assets/images/CocktailCoterie.PNG';
-import img2 from '../assets/images/WeatherDashboard.png';
-import img3 from '../assets/images/Webbie.PNG';
-import img4 from '../assets/images/PasswordGenerator.PNG';
+import img2 from '../assets/images/Webbie.PNG';
+import img3 from '../assets/images/SugarBoos.JPG';
+import img4 from '../assets/images/PVSLP1.JPG';
+import img5 from '../assets/images/ChangeConverter.JPG';
+import img6 from '../assets/images/PVSLP2.JPG';
+import img7 from '../assets/images/MCA.JPG';
 
 class Project extends Component {
   render() {
     return (
       <div>
         <br /><br />
-<CardDeck>
+<CardGroup>
+  <CardDeck>
         <Card>
-          <Card.Img className='projectImg' src={img1} />
+          <Card.Img className='projectImg' src={img1} thumbnail/>
           <Card.Body>
-            <Card.Title className='projectText'>Cocktail Coterie</Card.Title>
+            <Card.Title className='projectText'>React - Cocktail Coterie</Card.Title>
             <Card.Text variant='dark'>
             <a href="https://github.com/AuroraBrune/cocktail_coterie">GitHub</a> | <a href="https://cocktail-coterie.herokuapp.com/">Demo</a>
             </Card.Text>
-
           </Card.Body>
         </Card>
+
         <Card>
-          <Card.Img className='projectImg' src={img3} />
+          <Card.Img className='projectImg' src={img2} thumbnail/>
           <Card.Body>
-          <Card.Title className='projectText'>Timed Quiz</Card.Title>                  
+          <Card.Title className='projectText'>Javascript Timed Quiz</Card.Title>                  
             <Card.Text>
             <a href="https://github.com/petersvirtualservices/TimedQuiz">GitHub</a> | <a href="https://petersvirtualservices.github.io/TimedQuiz/">Demo</a>    
             </Card.Text>
           </Card.Body>
         </Card>
-        <Card>
-          <Card.Img className='projectImg' src={img2} />
-          <Card.Body>
-            <Card.Title className='projectText'>Weather API</Card.Title>
-            
-            <Card.Text>
-            <a href="https://github.com/petersvirtualservices/WeatherAPI">GitHub</a> | <a href="https://petersvirtualservices.github.io/WeatherAPI/">Demo</a>
-             </Card.Text>
 
-          </Card.Body>
-        </Card>
         <Card>
-          <Card.Img className='projectImg' src={img4} />
+          <Card.Img className='projectImg' src={img3} thumbnail/>
           <Card.Body>
-            <Card.Title className='projectText'>Password Generator</Card.Title>            
+            <Card.Title className='projectText'>HTML/CSS Landing Page</Card.Title>            
             <Card.Text>
-            <a href="https://github.com/petersvirtualservices/PasswordCreator">GitHub</a> | <a href="https://petersvirtualservices.github.io/PasswordCreator/">Demo</a>
+            <a href="sugarbooslandingpage">GitHub</a> | <a href="https://petersvirtualservices.github.io/sugarbooslandingpage/">Demo</a>
              </Card.Text>
-
           </Card.Body>
         </Card>
         </CardDeck>
+        <CardDeck>
+        <Card>
+          <Card.Img className='projectImg' src={img4} thumbnail/>
+          <Card.Body>
+            <Card.Title className='projectText'>HTML/CSS Landing Page</Card.Title>
+            <Card.Text variant='dark'>
+            <a href="https://github.com/petersvirtualservices/PVSLandingPage1">GitHub</a> | <a href="https://petersvirtualservices.github.io/PVSLandingPage1/">Demo</a>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        
+        <Card>
+          <Card.Img className='projectImg' src={img5} thumbnail/>
+          <Card.Body>
+            <Card.Title className='projectText'>Javascript Change Converter</Card.Title>            
+            <Card.Text>
+            <a href="https://github.com/petersvirtualservices/changeconverter">GitHub</a> | <a href="https://petersvirtualservices.github.io/changeconverter/">Demo</a>
+             </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Img className='projectImg' src={img6} thumbnail/>
+          <Card.Body>
+            <Card.Title className='projectText'>HTML/CSS Landing Page</Card.Title>            
+            <Card.Text>
+            <a href="https://github.com/petersvirtualservices/PVSLandingPage2">GitHub</a> | <a href="https://petersvirtualservices.github.io/PVSLandingPage2/">Demo</a>
+             </Card.Text>
+          </Card.Body>
+        </Card>
+        </CardDeck>
+        <CardDeck>
+        <Card>
+          <Card.Img className='projectImg' src={img7} thumbnail/>
+          <Card.Body>
+            <Card.Title className='projectText'>HTML/CSS/Javascript API</Card.Title>            
+            <Card.Text>
+            <a href="https://github.com/petersvirtualservices/movementconnectionanalyzer">GitHub</a> | <a href="https://petersvirtualservices.github.io/movementconnectionanalyzer/">Demo</a>
+             </Card.Text>
+          </Card.Body>
+        </Card>
+        </CardDeck>
+        </CardGroup>
       </div>
     )
   };
